2d28cd563dfc2a3a791494bede69d4f9eb78d89f
t2 <oo_ri@naver.com>
2021-03-10 02:47:09 +0900

    Update App.js

--- 01.expo/App.tsx
+++ 01.expo/App.tsx
@@ -1,21 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { memo } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  title: {
+    fontSize: 30,
   },
 });
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>React Native</Text>
+    </View>
+  );
+};
+
+export default memo(App);
