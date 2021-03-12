0e90196d96eb4d01826e38e27a9ed7c6d37780c5
t2 <oo_ri@naver.com>
2021-03-13 03:44:01 +0900

    Create React Native Style.ts

--- 03.style/App.tsx
+++ 03.style/App.tsx
@@ -1,15 +1,12 @@
 import React, { FC, memo } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
-const styles = StyleSheet.create({
-  container: { flex: 1, backgroundColor: '#fff', alignItems: 'center', justifyContent: 'center' },
-  text: { padding: 10, fontSize: 26, fontWeight: '600', color: 'black' },
-});
+import { textStyles, viewStyles } from './src/styles';
 
 const App: FC = () => {
   return (
-    <View style={styles.container}>
-      <Text style={[styles.text, { color: 'green' }]}>Inline Styling - Text</Text>
+    <View style={viewStyles.container}>
+      <Text style={textStyles.text}>Inline Styling - Text</Text>
     </View>
   );
 };
