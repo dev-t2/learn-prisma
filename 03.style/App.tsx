3ca720b2dd454fb96319a6548c2d20a8dafa2826
t2 <oo_ri@naver.com>
2021-03-13 03:23:12 +0900

    React Native Class Style Update

--- 03.style/App.tsx
+++ 03.style/App.tsx
@@ -1,14 +1,15 @@
 import React, { FC, memo } from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: { flex: 1, backgroundColor: '#fff', alignItems: 'center', justifyContent: 'center' },
+  text: { padding: 10, fontSize: 26, fontWeight: '600', color: 'black' },
+});
 
 const App: FC = () => {
   return (
-    <View
-      style={{ flex: 1, backgroundColor: '#fff', alignItems: 'center', justifyContent: 'center' }}
-    >
-      <Text style={{ padding: 10, fontSize: 26, fontWeight: '600', color: 'black' }}>
-        Inline Styling - Text
-      </Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Inline Styling - Text</Text>
     </View>
   );
 };
