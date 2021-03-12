546e75dad1c28118f91791695c021bafaf340352
t2 <oo_ri@naver.com>
2021-03-13 03:17:56 +0900

    React Native Inline Style Update

--- 03.style/App.tsx
+++ 03.style/App.tsx
@@ -1,21 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { FC, memo } from 'react';
+import { Text, View } from 'react-native';
 
-export default function App() {
+const App: FC = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View
+      style={{ flex: 1, backgroundColor: '#fff', alignItems: 'center', justifyContent: 'center' }}
+    >
+      <Text style={{ padding: 10, fontSize: 26, fontWeight: '600', color: 'black' }}>
+        Inline Styling - Text
+      </Text>
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default memo(App);
