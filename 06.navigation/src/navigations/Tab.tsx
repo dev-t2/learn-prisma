2d574912041887d8bad6bdc52701efc6a72c1a4d
t2 <oo_ri@naver.com>
2021-03-27 17:54:50 +0900

    Tab Navigation Material Community Icon Update

--- 06.navigation/src/navigations/Tab.tsx
+++ 06.navigation/src/navigations/Tab.tsx
@@ -1,16 +1,45 @@
 import React, { memo } from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 import { Mail, Meet, Settings } from '../screens';
 
 const Tab = createBottomTabNavigator();
 
+interface ITabIcon {
+  name: any;
+  size: number;
+  color: string;
+}
+
+const TabIcon = ({ name, size, color }: ITabIcon) => {
+  return <MaterialCommunityIcons name={name} size={size} color={color} />;
+};
+
 const TabNavigation = () => {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Mail" component={Mail} />
-      <Tab.Screen name="Meet" component={Meet} />
-      <Tab.Screen name="Settings" component={Settings} />
+      <Tab.Screen
+        name="Mail"
+        component={Mail}
+        options={{
+          tabBarIcon: props => TabIcon({ ...props, name: 'email' }),
+        }}
+      />
+      <Tab.Screen
+        name="Meet"
+        component={Meet}
+        options={{
+          tabBarIcon: props => TabIcon({ ...props, name: 'video' }),
+        }}
+      />
+      <Tab.Screen
+        name="Settings"
+        component={Settings}
+        options={{
+          tabBarIcon: props => TabIcon({ ...props, name: 'cog' }),
+        }}
+      />
     </Tab.Navigator>
   );
 };
