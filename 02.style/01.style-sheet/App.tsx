89b17a4552a2974c1d7a74fffa9957ce92554538
t2 <oo_ri@naver.com>
2021-05-14 02:36:17 +0900

    ESLint Install and Setup

--- 02.style/01.style-sheet/App.tsx
+++ 02.style/01.style-sheet/App.tsx
@@ -1,21 +1,15 @@
+import React, { memo } from 'react';
+import { SafeAreaView, Text } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    <SafeAreaView>
+      <StatusBar />
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+      <Text>Open up App.tsx to start working on your app!</Text>
+    </SafeAreaView>
+  );
+};
+
+export default memo(App);
