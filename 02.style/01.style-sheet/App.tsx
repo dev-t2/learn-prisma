1f938dc1865f757d4b1726a5bef8e336f8b849f9
t2 <oo_ri@naver.com>
2021-05-14 02:47:48 +0900

    Style update using StyleSheet API

--- 02.style/01.style-sheet/App.tsx
+++ 02.style/01.style-sheet/App.tsx
@@ -1,13 +1,20 @@
 import React, { memo } from 'react';
-import { SafeAreaView, Text } from 'react-native';
-import { StatusBar } from 'expo-status-bar';
+import { SafeAreaView, StyleSheet, Text } from 'react-native';
+
+const styles = StyleSheet.create({
+  safeAreaView: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'blue',
+  },
+  text: { fontSize: 20, color: 'white' },
+});
 
 const App = () => {
   return (
-    <SafeAreaView>
-      <StatusBar />
-
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <SafeAreaView style={styles.safeAreaView}>
+      <Text style={styles.text}>Hello StyleSheet API</Text>
     </SafeAreaView>
   );
 };
