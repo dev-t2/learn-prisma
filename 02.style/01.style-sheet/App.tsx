6cad8610089ed06875081901f216d526c3ce7259
t2 <oo_ri@naver.com>
2021-05-15 01:13:04 +0900

    Paper and Color Install and Style Update

--- 02.style/01.style-sheet/App.tsx
+++ 02.style/01.style-sheet/App.tsx
@@ -1,14 +1,22 @@
 import React, { memo } from 'react';
 import { SafeAreaView, StyleSheet, Text } from 'react-native';
+import { Colors } from 'react-native-paper';
+import Color from 'color';
+
+console.log(Colors.blue500);
+console.log(Color(Colors.blue500).alpha(0.5).lighten(0.5).string());
 
 const styles = StyleSheet.create({
   safeAreaView: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: 'blue',
+    backgroundColor: Colors.blue500,
   },
-  text: { fontSize: 20, color: 'white' },
+  text: {
+    fontSize: 20,
+    color: Color(Colors.blue500).alpha(0.5).lighten(0.5).string(),
+  },
 });
 
 const App = () => {
