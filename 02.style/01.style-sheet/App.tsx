8af1bb355c9e6940d8547f89a3403e3ff510120c
t2 <oo_ri@naver.com>
2021-05-15 01:21:38 +0900

    Styled Components Install and Style Update

--- 02.style/01.style-sheet/App.tsx
+++ 02.style/01.style-sheet/App.tsx
@@ -1,29 +1,25 @@
 import React, { memo } from 'react';
-import { SafeAreaView, StyleSheet, Text } from 'react-native';
 import { Colors } from 'react-native-paper';
+import styled from 'styled-components/native';
 import Color from 'color';
 
-console.log(Colors.blue500);
-console.log(Color(Colors.blue500).alpha(0.5).lighten(0.5).string());
+const StyledSafeAreaView = styled.SafeAreaView({
+  flex: 1,
+  alignItems: 'center',
+  justifyContent: 'center',
+  backgroundColor: Colors.blue500,
+});
 
-const styles = StyleSheet.create({
-  safeAreaView: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: Colors.blue500,
-  },
-  text: {
-    fontSize: 20,
-    color: Color(Colors.blue500).alpha(0.5).lighten(0.5).string(),
-  },
+const StyledText = styled.Text({
+  fontSize: 24,
+  color: Color(Colors.blue500).alpha(0.5).lighten(0.5).string(),
 });
 
 const App = () => {
   return (
-    <SafeAreaView style={styles.safeAreaView}>
-      <Text style={styles.text}>Hello StyleSheet API</Text>
-    </SafeAreaView>
+    <StyledSafeAreaView>
+      <StyledText>Hello StyleSheet API</StyledText>
+    </StyledSafeAreaView>
   );
 };
 
