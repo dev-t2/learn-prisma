f00d7d7c6c56c085a5c862fc822dbc5418530e77
t2 <oo_ri@naver.com>
2021-05-15 16:34:05 +0900

    ESLint, Styled Components Install and Project Setup

---
+++ 02.style/02.view/App.tsx
@@ -0,0 +1,12 @@
+import React, { memo } from 'react';
+import { Text, View } from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Open up App.tsx to start working on your app!</Text>
+    </View>
+  );
+};
+
+export default memo(App);
