98fc7f6fb42b803ca3abfb1940ba53300fbf0dae
t2 <oo_ri@naver.com>
2021-05-15 18:03:05 +0900

    Platform and useWindowDimensions Update

--- 02.style/02.view/App.tsx
+++ 02.style/02.view/App.tsx
@@ -1,11 +1,28 @@
 import React, { memo } from 'react';
-import { Text, View } from 'react-native';
+import { Platform, StatusBar, useWindowDimensions } from 'react-native';
+import { Colors } from 'react-native-paper';
+import styled from 'styled-components/native';
+
+const StyledSafeAreaView = styled.SafeAreaView({
+  backgroundColor: Colors.blue200,
+});
+
+const StyledText = styled.Text({
+  fontSize: 24,
+  color: Colors.blue900,
+});
 
 const App = () => {
+  const { width, height } = useWindowDimensions();
+
   return (
-    <View>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <StyledSafeAreaView>
+      <StatusBar />
+
+      <StyledText>os: {Platform.OS}</StyledText>
+      <StyledText>width: {width} px</StyledText>
+      <StyledText>height: {height} px</StyledText>
+    </StyledSafeAreaView>
   );
 };
 
