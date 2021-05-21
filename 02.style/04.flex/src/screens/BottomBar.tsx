3410872b33dbf9fb085f931e73749d2bb7b59cb4
t2 <oo_ri@naver.com>
2021-05-21 23:01:06 +0900

    BottomBar JustfyContent Style Update

--- 02.style/04.flex/src/screens/BottomBar.tsx
+++ 02.style/04.flex/src/screens/BottomBar.tsx
@@ -1,23 +1,36 @@
 import React, { memo } from 'react';
 import { Colors } from 'react-native-paper';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 import styled from 'styled-components/native';
 
 const StyledView = styled.View({
-  padding: 5,
-  backgroundColor: Colors.blue900,
+  flexDirection: 'row',
+  alignItems: 'center',
+  justifyContent: 'space-evenly',
+  padding: 10,
+  backgroundColor: Colors.blue500,
 });
 
-const StyledText = styled.Text({
-  fontSize: 20,
-  color: Colors.white,
-});
+const StyledMaterialCommunityIcons = styled(MaterialCommunityIcons)({});
 
-const TITLE = 'BottomBar';
+const icons = [
+  'home',
+  'table-search',
+  'face-profile-woman',
+  'account-settings',
+];
 
 const BottomBar = () => {
   return (
     <StyledView>
-      <StyledText>{TITLE}</StyledText>
+      {icons.map(name => (
+        <StyledMaterialCommunityIcons
+          key={name}
+          name={name}
+          size={30}
+          color="#ffffff"
+        />
+      ))}
     </StyledView>
   );
 };
