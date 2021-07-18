14b15d7987e53bf3e550e99e7bac95d08a4856f7
t2 <oo_ri@naver.com>
2021-07-18 11:29:26 +0900

    Inline Style Update

--- 03_style/01_style/App.tsx
+++ 03_style/01_style/App.tsx
@@ -5,17 +5,35 @@ import { StatusBar } from 'expo-status-bar';
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#fff',
   },
 });
 
 const App = () => {
   return (
-    <View style={styles.container}>
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#fff',
+      }}
+    >
       <StatusBar style="auto" />
-      <Text>Open up App.tsx to start working on your app!</Text>
+
+      <Text
+        style={{ padding: 10, fontSize: 25, fontWeight: '600', color: 'black' }}
+      >
+        Inline Style - Text
+      </Text>
+
+      <Text
+        style={{ padding: 10, fontSize: 25, fontWeight: '600', color: 'red' }}
+      >
+        Inline Style - Error
+      </Text>
     </View>
   );
 };
