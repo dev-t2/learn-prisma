84651cacedba5941446b71b0b06c198e78f02a2a
t2 <oo_ri@naver.com>
2021-07-24 11:36:39 +0900

    05_function.js Update

---
+++ 00_javascript/05_function.js
@@ -0,0 +1,17 @@
+function add1(a, b) {
+  return a + b;
+}
+console.log(add1(1, 2));
+
+function hello1(name) {
+  console.log(`Hello ${name}`);
+}
+hello1('JavaScript');
+
+const add2 = (a, b) => {
+  return a + b;
+};
+console.log(add2(1, 2));
+
+const add3 = (a, b) => a + b;
+console.log(add3(1, 2));
