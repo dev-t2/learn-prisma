6f2a9bd7bca9505e61035cd4361834911718e681
t2 <oo_ri@naver.com>
2021-07-24 16:14:10 +0900

    10_operator.js Update

---
+++ 00_javascript/10_operator.js
@@ -0,0 +1,11 @@
+const array = [];
+
+if (array.length === 0) {
+  console.log('배열이 비어 있습니다.');
+} else {
+  console.log('배열이 비어 있지 않습니다.');
+}
+
+array.length === 0
+  ? console.log('배열이 비어 있습니다.')
+  : console.log('배열이 비어 있지 않습니다.');
