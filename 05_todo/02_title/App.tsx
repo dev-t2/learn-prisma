9250c0053b3b5d278f465266a3d97d2d33983941
t2 <oo_ri@naver.com>
2021-07-31 16:56:14 +0900

    Status Bar Style and Title Component Update

--- 05_todo/02_title/App.tsx
+++ 05_todo/02_title/App.tsx
@@ -1,22 +1,27 @@
 import React, { memo } from 'react';
+import Constants from 'expo-constants';
 import { StatusBar } from 'expo-status-bar';
 import { ThemeProvider } from '@emotion/react';
 import styled from '@emotion/native';
 
 import { theme } from './src/emotion/theme';
+import { Title } from './src/components';
 
-const Container = styled.View(({ theme }) => ({
+const Container = styled.SafeAreaView(({ theme }) => ({
   flex: 1,
   alignItems: 'center',
-  justifyContent: 'center',
+  justifyContent: 'flex-start',
   backgroundColor: theme.background,
+  paddingTop: Constants.statusBarHeight,
 }));
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar style="light" />
+
       <Container>
-        <StatusBar style="auto" />
+        <Title>TODO</Title>
       </Container>
     </ThemeProvider>
   );
--- 05_todo/02_title/src/emotion/theme.ts
+++ 05_todo/02_title/src/emotion/theme.ts
@@ -3,7 +3,7 @@ import { Theme } from '@emotion/react';
 export const theme: Theme = {
   background: '#111',
   itemBackground: '#333',
-  main: '#778bdd',
+  main: '#1976d2',
   text: '#eee',
   done: '#666',
 };
