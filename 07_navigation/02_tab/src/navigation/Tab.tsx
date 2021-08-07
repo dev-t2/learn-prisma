80820cefe7ee894b49b3934544cd20249415e7b8
t2 <oo_ri@naver.com>
2021-08-07 16:25:21 +0900

    Bottom Tab Navigation Style Update

--- 07_navigation/02_tab/src/navigation/Tab.tsx
+++ 07_navigation/02_tab/src/navigation/Tab.tsx
@@ -1,16 +1,69 @@
-import React, { memo } from 'react';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import React, { memo, useMemo } from 'react';
+import { MaterialIcons } from '@expo/vector-icons';
+import {
+  BottomTabNavigationOptions,
+  createBottomTabNavigator,
+} from '@react-navigation/bottom-tabs';
 
 import { Mail, Profile, Settings } from '../screen/TabScreens';
+
+const TabIcon = ({
+  name,
+  size,
+  color,
+}: {
+  name: any;
+  size: number;
+  color: string;
+}) => <MaterialIcons name={name} size={size} color={color} />;
 
 const { Navigator, Screen } = createBottomTabNavigator();
 
 const Tab = () => {
+  const screenOptions = useMemo<BottomTabNavigationOptions>(
+    () => ({
+      tabBarShowLabel: true,
+      tabBarLabelPosition: 'below-icon',
+      tabBarStyle: {
+        backgroundColor: '#0f56b3',
+        borderTopColor: '#000',
+        borderTopWidth: 5,
+      },
+      tabBarActiveTintColor: '#fff',
+      tabBarInactiveTintColor: '#5096f1',
+    }),
+    []
+  );
+
+  const mailOptions = useMemo<BottomTabNavigationOptions>(
+    () => ({
+      tabBarIcon: ({ size, color, focused }) =>
+        TabIcon({ name: focused ? 'mail' : 'mail-outline', size, color }),
+      tabBarLabel: 'Inbox',
+    }),
+    []
+  );
+
+  const profileOptions = useMemo<BottomTabNavigationOptions>(
+    () => ({
+      tabBarIcon: ({ size, color }) => TabIcon({ name: 'person', size, color }),
+    }),
+    []
+  );
+
+  const settingsOptions = useMemo<BottomTabNavigationOptions>(
+    () => ({
+      tabBarIcon: ({ size, color }) =>
+        TabIcon({ name: 'settings', size, color }),
+    }),
+    []
+  );
+
   return (
-    <Navigator>
-      <Screen name="Mail" component={Mail} />
-      <Screen name="Profile" component={Profile} />
-      <Screen name="Settings" component={Settings} />
+    <Navigator screenOptions={screenOptions}>
+      <Screen name="Mail" component={Mail} options={mailOptions} />
+      <Screen name="Profile" component={Profile} options={profileOptions} />
+      <Screen name="Settings" component={Settings} options={settingsOptions} />
     </Navigator>
   );
 };
