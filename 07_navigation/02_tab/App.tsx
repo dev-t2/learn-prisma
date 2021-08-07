a3c2c4b99aeb838ad0fa6432f06eea7952a406e8
t2 <oo_ri@naver.com>
2021-08-07 15:18:40 +0900

    02_tab Update

---
+++ 07_navigation/02_tab/App.tsx
@@ -0,0 +1,17 @@
+import React, { memo } from 'react';
+
+import { StatusBar } from 'expo-status-bar';
+
+import { Navigation } from './src/navigation';
+
+const App = () => {
+  return (
+    <>
+      <StatusBar style="auto" />
+
+      <Navigation />
+    </>
+  );
+};
+
+export default memo(App);
