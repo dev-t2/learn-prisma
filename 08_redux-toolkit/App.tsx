894a39bc72d77c73a8e5b3db6ccb0ac78384c13d
t2 <oo_ri@naver.com>
2021-08-08 14:25:52 +0900

    ESLint Install and Update

--- 08_redux-toolkit/App.tsx
+++ 08_redux-toolkit/App.tsx
@@ -1,21 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { memo } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import { StatusBar } from 'expo-status-bar';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#fff',
   },
 });
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+
+      <Text>Open up App.tsx to start working on your app!</Text>
+    </View>
+  );
+};
+
+export default memo(App);
