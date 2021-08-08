8b2e0382a6ecbec897ea438ec6b2ac105a1908c1
t2 <oo_ri@naver.com>
2021-08-08 23:38:12 +0900

    React Navigation Install and Setup

--- 09_chat/App.tsx
+++ 09_chat/App.tsx
@@ -2,25 +2,18 @@ import React, { memo } from 'react';
 import { Provider } from 'react-redux';
 import { StatusBar } from 'expo-status-bar';
 import { ThemeProvider } from '@emotion/react';
-import styled from '@emotion/native';
 
 import { store } from './src/redux';
 import { theme } from './src/emotion';
-
-const Container = styled.View({
-  flex: 1,
-  backgroundColor: '#fff',
-  alignItems: 'center',
-  justifyContent: 'center',
-});
+import { Navigation } from './src/navigation';
 
 const App = () => {
   return (
     <Provider store={store}>
       <ThemeProvider theme={theme}>
-        <Container>
-          <StatusBar style="auto" />
-        </Container>
+        <StatusBar style="auto" />
+
+        <Navigation />
       </ThemeProvider>
     </Provider>
   );
---
+++ 09_chat/src/navigation/Navigation.tsx
@@ -0,0 +1,8 @@
+import React, { memo } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+
+const Navigation = () => {
+  return <NavigationContainer>Navigation Container</NavigationContainer>;
+};
+
+export default memo(Navigation);
