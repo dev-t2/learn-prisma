8abfd67922fdbbebfa8569d94e02029a71e271b1
t2 <oo_ri@naver.com>
2021-08-14 13:19:10 +0900

    SignUp Screen Options Update

--- 09_chat/src/navigation/Auth.tsx
+++ 09_chat/src/navigation/Auth.tsx
@@ -1,10 +1,12 @@
 import React, { memo, useMemo } from 'react';
+import { MaterialIcons } from '@expo/vector-icons';
 import { RouteProp } from '@react-navigation/native';
 import {
   createStackNavigator,
   StackNavigationOptions,
   StackNavigationProp,
 } from '@react-navigation/stack';
+import { useTheme } from '@emotion/react';
 
 import { SignIn, SignUp } from '../screen';
 
@@ -28,6 +30,8 @@ export type SignUpScreenNavigationProp = StackNavigationProp<
 const { Navigator, Screen } = createStackNavigator<RootStackParamList>();
 
 const Auth = () => {
+  const theme = useTheme();
+
   const signInOptions = useMemo<StackNavigationOptions>(
     () => ({
       headerShown: false,
@@ -35,10 +39,27 @@ const Auth = () => {
     []
   );
 
+  const signUpOptions = useMemo<StackNavigationOptions>(
+    () => ({
+      headerTitleAlign: 'center',
+      headerBackTitleVisible: false,
+      headerTintColor: theme.text,
+      headerLeft: ({ tintColor, onPress }) => (
+        <MaterialIcons
+          name="keyboard-arrow-left"
+          size={30}
+          color={tintColor}
+          onPress={onPress}
+        />
+      ),
+    }),
+    [theme.text]
+  );
+
   return (
     <Navigator>
       <Screen name="SignIn" component={SignIn} options={signInOptions} />
-      <Screen name="SignUp" component={SignUp} />
+      <Screen name="SignUp" component={SignUp} options={signUpOptions} />
     </Navigator>
   );
 };
