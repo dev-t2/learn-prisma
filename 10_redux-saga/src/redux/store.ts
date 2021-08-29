d26959c06768f06b97a0404b7953bf39a5249514
t2 <oo_ri@naver.com>
2021-08-29 22:58:32 +0900

    Redux Toolkit Install and Setup

---
+++ 10_redux-saga/src/redux/store.ts
@@ -0,0 +1,16 @@
+import { configureStore } from '@reduxjs/toolkit';
+import rootReducer from './rootReducer';
+
+const store = configureStore({
+  devTools: process.env.NODE_ENV === 'development',
+  middleware: [],
+  reducer: rootReducer,
+});
+
+if (process.env.NODE_ENV === 'development' && (module as any).hot) {
+  (module as any).hot.accept(() => {
+    store.replaceReducer(rootReducer);
+  });
+}
+
+export default store;
