bbd017a4fe82a85b68e305515b49f2464a51b495
t2 <oo_ri@naver.com>
2021-09-22 14:01:21 +0900

    App Loading Component Delete

--- 07_chat/App.tsx
+++ 07_chat/App.tsx
@@ -1,6 +1,4 @@
-import React, { memo, useCallback, useState } from 'react';
-import { Alert } from 'react-native';
-import AppLoading from 'expo-app-loading';
+import React, { memo } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { ThemeProvider } from '@emotion/react';
 
@@ -8,35 +6,11 @@ import { theme } from './src/emotion';
 import Navigation from './src/navigation';
 
 const App = () => {
-  const [isReady, setIsReady] = useState(false);
-
-  const startAsync = useCallback(async () => {
-    await Promise.all<any>([]);
-  }, []);
-
-  const onFinish = useCallback(() => {
-    setIsReady(true);
-  }, []);
-
-  const onError = useCallback((error: Error) => {
-    Alert.alert(error.message);
-  }, []);
-
   return (
     <ThemeProvider theme={theme}>
-      {isReady ? (
-        <AppLoading
-          startAsync={startAsync}
-          onFinish={onFinish}
-          onError={onError}
-        />
-      ) : (
-        <>
-          <StatusBar style="auto" />
+      <StatusBar style="auto" />
 
-          <Navigation />
-        </>
-      )}
+      <Navigation />
     </ThemeProvider>
   );
 };
