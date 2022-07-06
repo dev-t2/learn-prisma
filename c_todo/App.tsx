5dbebc3af84458d6a5705ddcdb395e7e319c3e1c
t2 <oo_ri@naver.com>
2022-07-06 10:24:49 +0900

    c_todo React Native Setup

---
+++ c_todo/App.tsx
@@ -0,0 +1,24 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React, { memo } from 'react';
+import { SafeAreaView, Text, View } from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>TodoApp</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default memo(App);
