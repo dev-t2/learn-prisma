5a01397cc0dd064adf6ce75e5ebc9c1ad22084ca
t2 <oo_ri@naver.com>
2022-07-07 16:34:39 +0900

    TextInput Component Style Update

--- c_todo/src/components/AddTodo.tsx
+++ c_todo/src/components/AddTodo.tsx
@@ -2,12 +2,23 @@ import React, { memo } from 'react';
 import styled from '@emotion/native';
 
 const Container = styled.View(({ theme }) => ({
-  height: 64,
-  backgroundColor: theme.colors.primary,
+  borderTopWidth: 1,
+  borderBottomWidth: 1,
+  borderColor: theme.colors.border,
 }));
 
+const StyledTextInput = styled.TextInput({
+  paddingVertical: 8,
+  paddingHorizontal: 16,
+  fontSize: 16,
+});
+
 const AddTodo = () => {
-  return <Container />;
+  return (
+    <Container>
+      <StyledTextInput placeholder="해야 할 일을 입력하세요" />
+    </Container>
+  );
 };
 
 export default memo(AddTodo);
