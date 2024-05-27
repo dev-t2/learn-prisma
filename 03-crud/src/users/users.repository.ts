db015b3c27c2990bf823f53bbfe74ec63b2c7390
dev-t2 <oo_ri@naver.com>
2024-05-27 23:39:46 +0900

    03. CRUD Transaction Update

--- 03-crud/src/users/users.repository.ts
+++ 03-crud/src/users/users.repository.ts
@@ -1,4 +1,4 @@
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { faker } from '@faker-js/faker/locale/ko';
 
 import { PrismaService } from 'src/prisma/prisma.service';
@@ -45,30 +45,44 @@ export class UsersRepository {
 
   async findUsers({ page, take }: FindUsersDto) {
     try {
-      return await this.prismaService.user.findMany({
-        // where: {
-        //   // email: {
-        //   //   // contains: 'gmail.com',
-        //   //   // not: { contains: 'gmail.com' },
-        //   // },
-        //   // userInfo: {
-        //   //   // userId: 10,
-        //   //   // userId: { in: [10, 20, 30] },
-        //   //   // userId: { notIn: [1, 3, 5] },
-        //   //   // userId: { gt: 10 },
-        //   //   // userId: { gte: 10 },
-        //   //   // userId: { lt: 10 },
-        //   //   // userId: { lte: 10 },
-        //   //   //phoneNumber: { contains: '040' },
-        //   // },
-        //   // AND: [{ id: { gte: 10 } }, { email: { contains: 'gmail' } }],
-        //   // OR: [{ email: { contains: 'naver' } }, { email: { contains: 'gmail' } }],
-        // },
-        orderBy: { id: 'asc' },
-        skip: take * (page - 1),
-        take,
-        select: { id: true, email: true, userInfo: { select: { phoneNumber: true, age: true } } },
-      });
+      // return await this.prismaService.user.findMany({
+      //   // where: {
+      //   //   // email: {
+      //   //   //   // contains: 'gmail.com',
+      //   //   //   // not: { contains: 'gmail.com' },
+      //   //   // },
+      //   //   // userInfo: {
+      //   //   //   // userId: 10,
+      //   //   //   // userId: { in: [10, 20, 30] },
+      //   //   //   // userId: { notIn: [1, 3, 5] },
+      //   //   //   // userId: { gt: 10 },
+      //   //   //   // userId: { gte: 10 },
+      //   //   //   // userId: { lt: 10 },
+      //   //   //   // userId: { lte: 10 },
+      //   //   //   //phoneNumber: { contains: '040' },
+      //   //   // },
+      //   //   // AND: [{ id: { gte: 10 } }, { email: { contains: 'gmail' } }],
+      //   //   // OR: [{ email: { contains: 'naver' } }, { email: { contains: 'gmail' } }],
+      //   // },
+      //   orderBy: { id: 'asc' },
+      //   skip: take * (page - 1),
+      //   take,
+      //   select: { id: true, email: true, userInfo: { select: { phoneNumber: true, age: true } } },
+      // });
+
+      const [users, totalUsers] = await this.prismaService.$transaction([
+        this.prismaService.user.findMany({
+          orderBy: { id: 'asc' },
+          skip: take * (page - 1),
+          take,
+          select: { id: true, email: true, userInfo: { select: { phoneNumber: true, age: true } } },
+        }),
+        this.prismaService.user.count(),
+      ]);
+
+      const totalPages = Math.ceil(totalUsers / take);
+
+      return { users, totalPages };
     } catch (e) {
       console.error(e);
 
@@ -78,10 +92,23 @@ export class UsersRepository {
 
   async findUser(id: number) {
     try {
-      return await this.prismaService.user.findUnique({
-        where: { id },
-        // include: { userInfo: true },
-        select: { id: true, email: true, userInfo: { select: { phoneNumber: true, age: true } } },
+      // return await this.prismaService.user.findUnique({
+      //   where: { id },
+      //   // include: { userInfo: true },
+      //   select: { id: true, email: true, userInfo: { select: { phoneNumber: true, age: true } } },
+      // });
+
+      await this.prismaService.$transaction(async (tx) => {
+        const user = await tx.user.findUnique({ where: { id }, select: { id: true, email: true } });
+
+        if (!user?.email.includes('naver')) {
+          throw new BadRequestException();
+        }
+
+        return await tx.user.findUnique({
+          where: { id: user.id + 1 },
+          select: { id: true, email: true, userInfo: { select: { phoneNumber: true, age: true } } },
+        });
       });
     } catch (e) {
       console.error(e);
